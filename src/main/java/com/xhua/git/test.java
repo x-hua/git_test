dd5df1a613a9e84a0ebddd7910257161c48f41f3
xhua <936761777@qq.com>
2022-07-08 10:28:04 +0800

    first com

---
+++ src/main/java/com/xhua/git/test.java
@@ -0,0 +1,14 @@
+package com.xhua.git;
+
+/**
+ * @author Shinelon
+ * @date 2022/7/8 10:24
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("aa");
+    }
+}
